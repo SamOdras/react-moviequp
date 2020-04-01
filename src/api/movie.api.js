9a38a06b0ac6a50087cad091ce8e965178357ce4
Sam Odras <odrassam@gmail.com>
2020-04-01 16:25:07 +0700

    Changing HTTP to HTTPS

--- src/api/movie.api.js
+++ src/api/movie.api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 export default Axios.create({
-  baseURL: 'http://www.omdbapi.com/?apikey=e3fad09a&s=',
+  baseURL: 'https://www.omdbapi.com/?apikey=e3fad09a&s=',
   timeout: 5000,
   headers: {
     'Access-Control-Allow-Origin':'*',
